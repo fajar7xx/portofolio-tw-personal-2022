b6b0453ced8dc9e3222ec1d3d5f9585a1f8dc44f
fajar7xx <fajar7xx@gmail.com>
2022-04-02 19:36:30 +0700

    portofolio

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// togle to make hamburger menu
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// navbar fixed when scroll down
+window.onscroll = () => {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
